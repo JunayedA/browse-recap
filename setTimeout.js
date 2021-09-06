3d05e7b48391c098456cd4889e8713835b1ea10d
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-06 16:33:43 +0600

    setTimeout recap

---
+++ setTimeout.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+    const num = 70;
+    console.log(num);
+},3500);
+
+// setTimeout(console.log('hi I am delay'),3500);
+
+// setTimeout(function(){
+//     console.log('js')
+// },3500);
